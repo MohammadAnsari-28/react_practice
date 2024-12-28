432d6ea0d7d97e9112f77b4c1e124044a8e10300
MohammadAnsari-28 <mohammadansari166@gmail.com>
2024-12-28 14:34:27 +0530

    initial set

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ExampleComponent from './components/ExampleComponent';
+import React from "react";
+import ExampleComponent from "./components/ExampleComponent";
 
 function App() {
-    return (
-        <div>
-            <h1>Welcome to My React App</h1>
-            <ExampleComponent />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <ExampleComponent name="Ansari" />
+    </div>
+  );
 }
 
 export default App;
